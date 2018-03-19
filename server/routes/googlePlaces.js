863d14b9ed560f810bf80c979eac7839cb7077e5
Mackenzie lee <kenzierlee23@outlook.com>
2018-03-19 09:00:54 -0600

    update api key

--- server/routes/googlePlaces.js
+++ server/routes/googlePlaces.js
@@ -13,14 +13,12 @@ var places = axios.create({
 
 router.post("/api/google", (req, res, next) => {
     console.log('reqBody', req.body)
-    console.log("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+req.body.midway.lat+","+req.body.midway.lng+"&rankby=distance&name="+req.body.category+"&key=AIzaSyCRj2cIdsswa8ob1lrGiHco48vVlV_mW0U")
-    google.get("json?location="+req.body.midway.lat+","+req.body.midway.lng+"&radius="+req.body.radius+"&name="+req.body.category+"&key=AIzaSyCRj2cIdsswa8ob1lrGiHco48vVlV_mW0U")
+    google.get("json?location="+req.body.midway.lat+","+req.body.midway.lng+"&radius="+req.body.radius+"&name="+req.body.category+"&key=AIzaSyBece11rk9XiCkYhO6GAL-DNC6_2p74_Ck")
         .then(response =>{
             var promises = []
             
             for (let i = 0; i < response.data.results.length; i++) {
                 const place = response.data.results[i];
-                console.log('json?placeid='+place.place_id+"&key=AIzaSyCRj2cIdsswa8ob1lrGiHco48vVlV_mW0U")
                 promises.push(places.get('json?placeid='+place.place_id+"&key=AIzaSyCRj2cIdsswa8ob1lrGiHco48vVlV_mW0U")
                 .then(res => {
                     return res.data.result
