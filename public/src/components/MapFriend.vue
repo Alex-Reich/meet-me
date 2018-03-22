9dede5c149cdac085f8c83500b96e96daa807564
Michael Lamb <503lamb@gmail.com>
2018-03-21 21:28:01 -0600

    save polyline for later

--- public/src/components/MapFriend.vue
+++ public/src/components/MapFriend.vue
@@ -36,3 +36,39 @@
         width: 450px;
     }
 </style> -->
+
+// POLYLINE
+
+directionsService.route(request, function (response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      var numberofWaypoints = response.routes[0].overview_path.length
+      var midPoint = response.routes[0].overview_path[parseInt(numberofWaypoints / 2)]
+      scope.roadMidwayMarker({
+        lat: midPoint.lat(),
+        lng: midPoint.lng()
+      }, scope.map)
+    }
+scope.createPolyline(response) // ADDED
+        })
+      },
+      createPolyline(directionResults) { // vvvv ADDED
+          console.log('DIRECTION RESULTS', directionResults)
+        if (this.roadDirectionLine.length > 0) {
+          var directLine = this.roadDirectionLine
+          for (var i = 0; i < directLine.length; i++) {
+            var polyline = directLine[i]
+            this.deletePolyline(polyline)
+          }
+        }
+        var line = new google.maps.Polyline({
+          path: directionResults.routes[0].overview_path,
+          strokeOpacity: .95,
+          strokeColor: '#3c6cae',
+          strokeWeight: 10,
+        })
+        line.setMap(this.map)
+        this.roadDirectionLine.push(line);
+      },
+      deletePolyline(polyline){
+        polyline.setMap(null)
+        this.roadDirectionLine = []
