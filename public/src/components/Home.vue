049148abdb68f5a4b55f0206defb28f373e52d8d
Michael Lamb <503lamb@gmail.com>
2018-03-25 15:06:56 -0600

    some styling

--- public/src/components/Home.vue
+++ public/src/components/Home.vue
@@ -1,109 +1,124 @@
 <template>
   <div class="home">
-  
-<navbar></navbar>
-
-</navbar>
-    <div class="container-fluid">
+    <navbar></navbar>
+    <div class="full">
       <div>
-        <div class="row d-flex justify-content-end">
-            
-        </div>
-        <div class="row spacer"></div>
-          <h1>Welcome to "Meet Me @..."</h1>
-          <div class="row spacer"></div>
-          <img src="../assets/meet-me-logo.png" height="200">
-
+        <img src="../assets/meet-me-logo.png" height="300">
       </div>
-      <div class="row spacer"></div>
-     
-      <div class="row d-flex justify-content-center ">
-        <router-link to="/meet-friend"  class="colfmt d-flex align-items-center justify-content-center col-lg-4 m-4">
-            <div>
-                <h2>Meet a Friend</h2>
-            </div>
+      <div>
+        <h3>Don't know where to meet?</h3>
+        <h3  class="pad-bot">Let us worry about that.</h3>
+        <router-link to="/meet-friend">
+          <button class="btn btn-block btn-outline-light">Meet a Friend</button>
         </router-link>
-        <div class="colfmt d-flex align-items-center justify-content-center m-4 col-lg-4"><h2>Plan a Trip</h2></div>
       </div>
-   
-  </div>
-  
-  
-  
+    </div>
+    <!-- <div class="row d-flex justify-content-center ">
+        <div class="colfmt d-flex align-items-center justify-content-center m-4 col-lg-4">
+            <h2>Plan a Trip</h2>
+        </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-    import Contacts from './Contacts'
-    import Navbar from './Navbar'
+  import Contacts from './Contacts'
+  import Navbar from './Navbar'
 
-    export default {
-        name: 'Home',
-        data() {
-            return {
-                user: {},
-                createdUser: {}
-            }
-        },
-        components: {
-            // login: Login,
-            contacts: Contacts,
-            navbar: Navbar
-        },
-        methods: {
-            // login() {
-            //     this.$store.dispatch('login', this.user)
-            //     $('#login').modal('hide')
-            // },
-            // createUser(createdUser) {
-            //     this.$store.dispatch('createUser', this.createdUser)
-            //     $('#signup').modal('hide')
-            // },
-            meetFriend() {
-                this.$store.dispatch('meetFriend', this.user)
-                $('#meetFriend').modal('hide')
-            },
-            manageContacts() {
-                this.$store.dispatch('getContacts', this.user)
-                $('#contacts').modal('hide')
-            }
-        }
-
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        user: {},
+        createdUser: {}
+      }
+    },
+    components: {
+      // login: Login,
+      contacts: Contacts,
+      navbar: Navbar
+    },
+    methods: {
+      // login() {
+      //     this.$store.dispatch('login', this.user)
+      //     $('#login').modal('hide')
+      // },
+      // createUser(createdUser) {
+      //     this.$store.dispatch('createUser', this.createdUser)
+      //     $('#signup').modal('hide')
+      // },
+      meetFriend() {
+        this.$store.dispatch('meetFriend', this.user)
+        $('#meetFriend').modal('hide')
+      },
+      manageContacts() {
+        this.$store.dispatch('getContacts', this.user)
+        $('#contacts').modal('hide')
+      }
     }
+
+  }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .home {
-        background-image: url("../assets/bg-meet-me.jpg");
-        background-position: center;
-        background-size: cover;
-        height: 100vh;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        color: white;
-    }
-    
-    .colfmt {
-        border: 2px solid white;
-        min-height: 20vh;
-        background-color: #4682b4;
-    }
-    
-    h1 {
-        font-size: 36pt;
-    }
-    
-    h2 {
-        color: white;
-    }
-    
-    .spacer {
-        min-height: 75px;
-    }
-    
-    router-link {
-        color: white;
-    }
+  .full {
+    height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin: 0 10%
+  }
+
+  .aligner {
+    align-items: center;
+  }
+
+  .pad-bot {
+      padding-bottom: 1rem;
+  }
+
+  .home {
+    background-image: url("../assets/bg-meet-me.jpg");
+    background-position: center;
+    background-size: cover;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: white;
+  }
+
+  .colfmt {
+    border: 2px solid white;
+    min-height: 20vh;
+    background-color: #4682b4;
+  }
+
+  h1 {
+    font-size: 36pt;
+    font-family: 'Lalezar', sans-serif;
+  }
+
+  h2 {
+    color: white;
+  }
+
+  .spacer {
+    min-height: 75px;
+  }
+
+  .flexor {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  router-link {
+    color: white;
+  }
+  a {
+      text-decoration: none;
+  }
+
 </style>
--- public/src/components/Navbar.vue
+++ public/src/components/Navbar.vue
@@ -1,209 +1,219 @@
 <template>
-    <div>
-        <nav class="navbar">
-            <div class="flexor">
-                <router-link :to="{name: 'Home'}">
-                    <div>
-                        <img class="pad-r" src="../assets/nav-logo.png" height="40px">
-                    </div>
-                </router-link>
+  <div>
+    <nav class="navbar">
+      <div class="flexor">
+        <router-link :to="{name: 'Home'}">
+          <div>
+            <img class="pad-r" src="../assets/nav-logo.png" height="40px">
+          </div>
+        </router-link>
+      </div>
+      <div v-if="!user._id">
+        <div class="dropdown pointer">
+          <a class="dropdown-toggle" role="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span>
+              <i class="fas fa-sign-in-alt"></i>
+            </span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+            <router-link to="/meet-friend">
+              <div class="dropdown-item pointer">
+                Meet a Friend
+              </div>
+            </router-link>
+            <div class="dropdown-item pointer ddcss" data-toggle="modal" data-target="#signUp">
+              Sign Up
             </div>
-            <div v-if="!user._id">
-                <div class="dropdown pointer">
-                    <a class="dropdown-toggle" role="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span>
-                            <i class="fas fa-sign-in-alt"></i>
-                        </span>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                        <button type="button" class="dropdown-item pointer ddcss" data-toggle="modal" data-target="#signUp">
-                            Sign Up
-                        </button>
-                        <button type="button" class="dropdown-item pointer ddcss" data-toggle="modal" data-target="#login">
-                            Login
-                        </button>
-
-                    </div>
-                </div>
-            </div>
-
-            <div v-else>
-                <div class="dropdown pointer">
-                    <a class="dropdown-toggle" role="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span>{{user.email}}
-                            <i class="fas fa-user"></i>
-                        </span>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                        <button type="button">
-                            <router-link to="/Contacts" class="dropdown-item pointer ddcss">
-                                Manage Contacts
-                            </router-link>
-                        </button>
-                        <button @click.prevent="logout" class="dropdown-item pointer ddcss" type="button">Logout</button>
-                    </div>
-                </div>
+            <div class="dropdown-item pointer ddcss" data-toggle="modal" data-target="#login">
+              Login
             </div>
 
-        </nav>
-        <div class="modal fade" id="signUp" tabindex="-1" role="dialog">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <form @submit.prevent="createUser(createdUser)">
-                        <div class="modal-body">
-                            <div class="m-2">
-                                <i class="fas mar-right fa-1x fa-user"></i>
-                                <input type="text" name="username" v-model="createdUser.userName" placeholder=" Username" required>
-                            </div>
-                            <div class="m-2">
-                                <i class="far mar-right fa-1x fa-envelope"></i>
-                                <input type="email" name="userEmail" v-model="createdUser.email" placeholder="Email" required>
-                            </div>
-                            <div class="m-2">
-                                <i class="far mar-right fa-1x fa-edit"></i>
-                                <input type="password" name="password" v-model="createdUser.password" placeholder=" Password">
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                                <b>close</b>
-                            </button>
-                            <button type="submit" class="btn submitBtn">
-                                <b>Create User</b>
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+          </div>
         </div>
-        <!-- Login Modal Start -->
-        <div class="modal fade" id="login" tabindex="-1" role="dialog">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <form @submit.prevent="login(user)">
-                        <div class="modal-body">
-                            <div class="m-2">
-                                <i class="far mar-right fa-1x fa-envelope"></i>
-                                <input type="text" name="userEmail" v-model="user.email" placeholder=" Email" required>
-                            </div>
-                            <div class="m-2">
-                                <i class="far mar-right fa-1x fa-edit"></i>
-                                <input type="password" name="password" v-model="user.password" placeholder=" Password">
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="submit" class="btn submitBtn">
-                                <b>Login</b>
-                            </button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                                <b>close</b>
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+      </div>
+      <div v-else>
+        <div class="dropdown pointer">
+          <a class="dropdown-toggle" role="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span>{{user.email}}
+              <i class="fas fa-user"></i>
+            </span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+            <router-link to="/meet-friend">
+              <div class="dropdown-item pointer">
+                Meet a Friend
+              </div>
+            </router-link>
+            <router-link to="/Contacts" class="dropdown-item pointer ddcss">
+              Manage Contacts
+            </router-link>
+            <button @click.prevent="logout" class="dropdown-item pointer ddcss" type="button">Logout</button>
+          </div>
         </div>
+      </div>
+
+    </nav>
+    <div class="modal fade" id="signUp" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <form @submit.prevent="createUser(createdUser)">
+            <div class="modal-body">
+              <div class="m-2">
+                <i class="fas mar-right fa-1x fa-user"></i>
+                <input type="text" name="username" v-model="createdUser.userName" placeholder=" Username" required>
+              </div>
+              <div class="m-2">
+                <i class="far mar-right fa-1x fa-envelope"></i>
+                <input type="email" name="userEmail" v-model="createdUser.email" placeholder="Email" required>
+              </div>
+              <div class="m-2">
+                <i class="far mar-right fa-1x fa-edit"></i>
+                <input type="password" name="password" v-model="createdUser.password" placeholder=" Password">
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                <b>close</b>
+              </button>
+              <button type="submit" class="btn submitBtn">
+                <b>Create User</b>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
+    <!-- Login Modal Start -->
+    <div class="modal fade" id="login" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <form @submit.prevent="login(user)">
+            <div class="modal-body">
+              <div class="m-2">
+                <i class="far mar-right fa-1x fa-envelope"></i>
+                <input type="text" name="userEmail" v-model="user.email" placeholder=" Email" required>
+              </div>
+              <div class="m-2">
+                <i class="far mar-right fa-1x fa-edit"></i>
+                <input type="password" name="password" v-model="user.password" placeholder=" Password">
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="submit" class="btn submitBtn">
+                <b>Login</b>
+              </button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                <b>close</b>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import Home from "./Home"
-    export default {
-        name: 'Navbar',
-        data() {
-            return {
-                createdUser: {}
-            }
-        },
-        computed: {
-            user() {
-                return this.$store.state.user
-            }
-        },
-        methods: {
-            logout() {
-                this.$store.dispatch('logout')
-            },
-            login() {
-                this.$store.dispatch('login', this.user)
-                this.$store.dispatch('authenticate')
-                $('#login').modal('hide')
-            },
-            createUser(createdUser) {
-                this.$store.dispatch('createUser', this.createdUser)
-                $('#signUp').modal('hide')
-                this.createdUser = {}
-            },
-            meetFriend() {
-                this.$store.dispatch('meetFriend', this.user)
-                $('#meetFriend').modal('hide')
-            },
-            manageContacts() {
-                this.$store.dispatch('getContacts', this.user)
-                $('#contacts').modal('hide')
-            }
-        }
+  import Home from "./Home"
+  export default {
+    name: 'Navbar',
+    data() {
+      return {
+        createdUser: {}
+      }
+    },
+    computed: {
+      user() {
+        return this.$store.state.user
+      }
+    },
+    methods: {
+      logout() {
+        this.$store.dispatch('logout')
+      },
+      login() {
+        this.$store.dispatch('login', this.user)
+        this.$store.dispatch('authenticate')
+        $('#login').modal('hide')
+      },
+      createUser(createdUser) {
+        this.$store.dispatch('createUser', this.createdUser)
+        $('#signUp').modal('hide')
+        this.createdUser = {}
+      },
+      meetFriend() {
+        this.$store.dispatch('meetFriend', this.user)
+        $('#meetFriend').modal('hide')
+      },
+      manageContacts() {
+        this.$store.dispatch('getContacts', this.user)
+        $('#contacts').modal('hide')
+      }
     }
+  }
+
 </script>
 
 
 <style scoped>
-    #must-login {
-        display: none;
-    }
+  #must-login {
+    display: none;
+  }
 
-    a {
-        color: #fff;
-    }
+  a {
+    color: #fff;
+  }
 
-    a:hover {
-        text-decoration: none
-    }
+  a:hover {
+    text-decoration: none
+  }
 
-    .pointer {
-        cursor: pointer;
-    }
+  .pointer {
+    cursor: pointer;
+  }
 
-    .navbar {
-        background-color: #3c6cae;
-        /* background-color: #7BA6D0; */
-        color: white;
-    }
+  .navbar {
+    background-color: #3c6cae;
+    /* background-color: #7BA6D0; */
+    color: white;
+    height: 10vh;
+  }
 
-    .navbar h3 {
-        font-family: 'Paytone One', sans-serif;
-        font-weight: 800;
-        font-size: 2rem;
-    }
+  .navbar h3 {
+    font-family: 'Paytone One', sans-serif;
+    font-weight: 800;
+    font-size: 2rem;
+  }
 
-    .ddcss {
-        color: black;
-        background-color: white;
-        font-style: normal;
-    }
+  .ddcss {
+    color: black;
+    background-color: white;
+    font-style: normal;
+  }
 
-    .dropdown-menu {
-        min-width: 0;
-        padding: 1rem 0;
-        margin-right: 10px;
-        left: -8rem;
-    }
+  .dropdown-menu {
+    min-width: 0;
+    padding: 1rem 0;
+    margin-right: 10px;
+    left: -8rem;
+  }
 
-    .dropdown-item:hover {
-        background-color: #f9f9f9;
-    }
+  .dropdown-item:hover {
+    background-color: #f9f9f9;
+  }
 
-    .flexor {
-        display: flex;
-        align-items: flex-end;
-        justify-content: space-around;
-    }
+  .flexor {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-around;
+  }
 
-    .flexor h3 {
-        margin-bottom: 0;
-    }
+  .flexor h3 {
+    margin-bottom: 0;
+  }
 
-    .pad-r {
-        padding-right: 1rem;
-    }
+  .pad-r {
+    padding-right: 1rem;
+  }
+
 </style>
